250083b5341e0e1677672eac0ada8dc4e2b2ecea
sauld08 <sauldirinot@gmail.com>
2025-09-20 17:34:23 -0400

    vista de detalles agregado

--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 class PostListView(ListView):
     model = Post
     template_name = "post_list.html"
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
     
