6565c1d3b75b961ae7b6346d505bd5d4fd70588d
sauld08 <sauldirinot@gmail.com>
2025-09-20 18:08:02 -0400

    archivo base agregado

--- blogs/models.py
+++ blogs/models.py
@@ -1,15 +1,13 @@
 from django.db import models
- 
+
+
 # Create your models here :).
 class Post(models.Model):
     title = models.CharField(max_length=200)
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(
-                            'auth.User',
-                            on_delete=models.CASCADE
-                            )
-    
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+
     def __str__(self):
         return self.title
--- blogs/urls.py
+++ blogs/urls.py
@@ -3,6 +3,6 @@ from .views import PostListView, PostDetailView
 
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list'),
-    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail")
+    path("", PostListView.as_view(), name="post_list"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]
