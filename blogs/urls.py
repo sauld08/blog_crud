6ac57f5fd29ea802fcc2e3f8c03430e40880e031
sauld08 <sauldirinot@gmail.com>
2025-10-04 16:45:57 -0400

    borrar post agregado

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView
+from .views import (
+                    PostListView,
+                    PostDetailView,
+                    PostCreateView,
+                    PostUpdateView,
+                    PostDeleteView,)
+
 
 
 urlpatterns = [
@@ -7,4 +13,5 @@ urlpatterns = [
     path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
     path("post/new", PostCreateView.as_view(), name="post-new"),
     path("post/<int:pk>/editar", PostUpdateView.as_view(), name="post-edit"),
+    path("post/<int:pk>/borrar", PostDeleteView.as_view(), name="post-delete" ),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from .models import Post
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import (
+                            CreateView,
+                            UpdateView,
+                            DeleteView,
+                            )
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -24,3 +29,8 @@ class PostUpdateView(UpdateView):
     model = Post
     template_name = 'post_update.html'
     fields = ['title', 'body']
+
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy("post_list")
